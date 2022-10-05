a74543698e6aa08ddddcd8e07e4014d10799c0b9
PC-Casa <arabsamir2003@gmail.com>
2022-10-05 07:51:17 -0300

    TP2EJ7

--- Ejercicios PHP2-7.php
+++ Ejercicios PHP2-7.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php - TP 2 Ejercicio 8</title>
+    <title>Php - TP 2 Ejercicio 7</title>
 </head>
 <body>
     
